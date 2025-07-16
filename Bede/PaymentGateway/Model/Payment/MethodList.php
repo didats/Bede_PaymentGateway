fd059c04ac78096d5df9c843ca1e0bd04e62349c
Didats Triadi <didats@gmail.com>
2025-07-16 10:11:03 +0700

    Update renderer and Method list

--- Bede/PaymentGateway/Model/Payment/MethodList.php
+++ Bede/PaymentGateway/Model/Payment/MethodList.php
@@ -1,16 +1,26 @@
 <?php
+
 namespace Bede\PaymentGateway\Model\Payment;
 
+use Bede\PaymentGateway\Helper\Data;
 use Bede\PaymentGateway\Model\Payment\Bede;
 
 class MethodList
 {
     protected $bede;
-    
-    public function __construct(Bede $apiclient) {
-        $this->bede = $apiclient;
+
+    public function __construct()
+    {
+        $helper = new Data();
+
+        $this->bede = new Bede();
+        $this->bede->merchantID = $helper->getMerchantId();
+        $this->bede->secretKey = $helper->getSecretKey();
+        $this->bede->successURL = $helper->getSuccessUrl();
+        $this->bede->failureURL = $helper->getFailureUrl();
+        $this->bede->subMerchantID = $helper->getSubmerchantUid();
     }
-    
+
     public function getAvailableMethods()
     {
         // Call your API to get available methods
--- Bede/PaymentGateway/view/frontend/web/js/view/payment/method-renderer/bede_payment.js
+++ Bede/PaymentGateway/view/frontend/web/js/view/payment/method-renderer/bede_payment.js
@@ -1,58 +1,3 @@
-// define(
-//     [
-//         'jquery',
-//         'Magento_Checkout/js/view/payment/default',
-//         'Magento_Checkout/js/action/select-payment-method',
-//         'Magento_Checkout/js/checkout-data'
-//     ], function ($, Component, selectPaymentMethodAction, checkoutData) {
-//         'use strict';
-
-//         console.log('Bede Payment Renderer loaded!');
-
-//         return Component.extend({
-//             defaults: {
-//                 template: 'Bede_PaymentGateway/payment/bede_template',
-//             },
-
-//             initialize: function () {
-//                 this._super();
-//                 console.log('Bede Payment initialized');
-//                 return this;
-//             }
-//         });
-
-//         // return Component.extend({
-//         //     defaults: {
-//         //         template: 'Bede_PaymentGateway/payment/bede_template',
-//         //         // availableMethods: []
-//         //     },
-
-
-// initObservable: function () {
-//     this._super()
-//         .observe('availableMethods');
-//     return this;
-// },
-
-// selectPaymentMethod: function () {
-//     var self = this;
-//     selectPaymentMethodAction(this.getData());
-//     checkoutData.setSelectedPaymentMethod(this.item.method);
-
-//     // Fetch available methods when selected
-//     $.getJSON('/bede_paymentgateway/payment/methods', function (response) {
-//         if (response.success) {
-//             self.availableMethods(response.methods);
-//         }
-//     });
-
-//     return true;
-// }
-
-//         // });
-//     }
-// );
-
 define([
     'jquery',
     'Magento_Checkout/js/view/payment/default',
@@ -89,22 +34,22 @@ define([
         },
 
         loadPaymentMethods: function () {
-            // var self = this;
-            // var serviceUrl = urlBuilder.createUrl('/bede_payment/payment/methods', {});
-            // $.ajax({
-            //     url: serviceUrl,
-            //     type: 'GET',
-            //     data: {
-            //         cartId: quote.quoteId
-            //     }
-            // }).done(function (response) {
-            //     if (response.success) {
-            //         self.availableMethods(response.methods);
-            //         if (response.methods.length > 0) {
-            //             self.selectedMethod(response.methods[0].value);
-            //         }
-            //     }
-            // });
+            var self = this;
+            var serviceUrl = require('mage/url').build('bede_paymentgateway/payment/methods');
+            $.ajax({
+                url: serviceUrl,
+                type: 'GET',
+                data: {
+                    cartId: quote.quoteId
+                }
+            }).done(function (response) {
+                if (response.success) {
+                    self.availableMethods(response.methods);
+                    if (response.methods.length > 0) {
+                        self.selectedMethod(response.methods[0].value);
+                    }
+                }
+            });
 
             return true;
         },
