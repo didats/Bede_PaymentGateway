2b99942e2a24ad09f7784749166d496a2fc64773
Didats Triadi <didats@gmail.com>
2025-07-17 15:31:51 +0700

    Payment method and country dial code

--- Bede/PaymentGateway/Model/PaymentMethod.php
+++ Bede/PaymentGateway/Model/PaymentMethod.php
@@ -2,6 +2,11 @@
 
 namespace Bede\PaymentGateway\Model;
 
+use Bede\PaymentGateway\Model\Payment\Bede;
+use Bede\PaymentGateway\Model\Payment\BedeBuyer;
+use Bede\PaymentGateway\Model\LogFactory;
+use Bede\PaymentGateway\Helper\Data;
+
 /**
  * Pay In Store payment method model
  */
@@ -17,6 +22,22 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
     protected $_isOffline = false;
     protected $_canAuthorize = true;
     protected $_canCapture = true;
+    protected $logFactory;
+    protected $bede;
+    protected $buyer;
+
+    public function __construct(Data $helper, Bede $bede, LogFactory $logFactory, BedeBuyer $buyer)
+    {
+        $this->logFactory = $logFactory;
+        $this->bede = $bede;
+        $this->buyer = $buyer;
+
+        $this->bede->merchantID = $helper->getMerchantId();
+        $this->bede->secretKey = $helper->getSecretKey();
+        $this->bede->successURL = $helper->getSuccessUrl();
+        $this->bede->failureURL = $helper->getFailureUrl();
+        $this->bede->subMerchantID = $helper->getSubmerchantUid();
+    }
 
     public function isAvailable(\Magento\Quote\Api\Data\CartInterface $quote = null)
     {
@@ -39,6 +60,9 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
         $payment = $this->getInfoInstance();
         $quote = $payment->getQuote();
 
+        $methodCode = $payment->getMethod();
+        $selectedSubmethod = $payment->getAdditionalInformation('selected_submethod');
+
         if ($quote) {
             // Total amount
             $grandTotal = $quote->getGrandTotal();
@@ -48,7 +72,25 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
             $billingAddress = $quote->getBillingAddress();
             $firstName = $billingAddress->getFirstname();
             $lastName = $billingAddress->getLastname();
-            // ...other fields as needed
+            $countryCode = $billingAddress->getCountryId();
+
+
+            $this->buyer->amount = $grandTotal;
+            $this->buyer->email = $customerEmail;
+            $this->buyer->name = $firstName . " " . $lastName;
+            $this->buyer->countryCode = $this->buyer->countryDialCode($countryCode);
+            $this->buyer->customerData1 = $customerEmail;
+            $this->buyer->customerData2 = $billingAddress;
+            $this->buyer->orderID = $quote->getId();
+            $response = $this->bede->requestLink($this->buyer, $selectedSubmethod);
+
+            $requestLog = $this->logFactory->create();
+            $requestLog->setData($this->bede->requestLogger);
+            $requestLog->save();
+
+            $responseLog = $this->logFactory->create();
+            $responseLog->setData($this->bede->responseLogger);
+            $responseLog->save();
         }
 
         return $this;
