75762f18911ce5ac942b68850f4abf44412b6d67
rezaarief89 <reza.arief@sirclo.com>
2025-07-19 19:50:19 +0700

    fix error construct in checkout page

--- Bede/PaymentGateway/Model/PaymentMethod.php
+++ Bede/PaymentGateway/Model/PaymentMethod.php
@@ -73,7 +73,7 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
         \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
         \Magento\Payment\Helper\Data $paymentData,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Payment\Model\Method\Logger $logger = null,
+        \Magento\Payment\Model\Method\Logger $logger,
         \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
         \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
         array $data = []
@@ -117,67 +117,4 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
 
         return $this;
     }
-
-    // public function assignData(\Magento\Framework\DataObject $data, Data $helper)
-    // {
-    //     parent::assignData($data);
-
-    //     $additionalData = $data->getData('additional_data');
-    //     if (isset($additionalData['selected_submethod'])) {
-    //         $this->getInfoInstance()->setAdditionalInformation(
-    //             'selected_submethod',
-    //             $additionalData['selected_submethod']
-    //         );
-    //     }
-
-    //     // Get quote from payment info instance
-    //     $payment = $this->getInfoInstance();
-    //     $quote = $payment->getQuote();
-
-    //     $methodCode = $payment->getMethod();
-    //     $selectedSubmethod = $payment->getAdditionalInformation('selected_submethod');
-
-    //     if ($quote) {
-    //         // Total amount
-    //         $grandTotal = $quote->getGrandTotal();
-
-    //         // Buyer data
-    //         $customerEmail = $quote->getCustomerEmail();
-    //         $billingAddress = $quote->getBillingAddress();
-    //         $firstName = $billingAddress->getFirstname();
-    //         $lastName = $billingAddress->getLastname();
-    //         $countryCode = $billingAddress->getCountryId();
-
-    //         // $bede = new Bede();
-    //         // $bede->merchantID = $helper->getMerchantId();
-    //         // $bede->secretKey = $helper->getSecretKey();
-    //         // $bede->successURL = $helper->getSuccessUrl();
-    //         // $bede->failureURL = $helper->getFailureUrl();
-    //         // $bede->subMerchantID = $helper->getSubmerchantUid();
-
-    //         // $buyer = new BedeBuyer();
-    //         // $buyer->amount = $grandTotal;
-    //         // $buyer->email = $customerEmail;
-    //         // $buyer->name = $firstName . " " . $lastName;
-    //         // $buyer->countryCode = $buyer->countryDialCode($countryCode);
-    //         // $buyer->customerData1 = $customerEmail;
-    //         // $buyer->customerData2 = $billingAddress;
-    //         // $buyer->orderID = $quote->getId();
-    //         // $response = $bede->requestLink($buyer, $selectedSubmethod);
-
-    //         // $requestLog = $this->logFactory->create();
-    //         // $requestLog->setData($this->bede->requestLogger);
-    //         // $requestLog->save();
-
-    //         // $responseLog = $this->logFactory->create();
-    //         // $responseLog->setData($this->bede->responseLogger);
-    //         // $responseLog->save();
-
-    //         // if (isset($resposne['url'])) {
-    //         //     // redirect the user to the url from API
-    //         // }
-    //     }
-
-    //     return $this;
-    // }
 }
